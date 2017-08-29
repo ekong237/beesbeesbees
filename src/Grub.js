677357f90a727684c7dc0f568627d09da15bc8d2
Elizabeth Kong <lizkong@Elizabeths-MacBook-Pro.local>
2017-08-28 17:39:56 -0700

    bees

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,15 @@
 var Grub = function() {
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
 };
 
+Grub.prototype.eat = function(){
+
+};
+
+
+var mygrub = new Grub(7);
+console.log(mygrub);
